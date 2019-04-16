8eedec1579aeb597ee47b2713121d97d22e5c201
Maria V <mariatua2@gmail.com>
2019-04-16 16:46:23 -0400

    at least it finds the solution

--- reto2.py
+++ reto2.py
@@ -6,126 +6,96 @@ import sys
 
 #Note: This is python3 
 
-region = [
-    [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-    [-1,-3,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1],
-    [-1,0,0,-1,0,-1,-1,-1,-1,-1,0,-1,-1,-1,-1,-1,0,-1,0,-1],
-    [-1,0,-1,-1,0,-1,0,0,0,-1,0,0,0,0,0,-1,0,0,0,-1],
-    [-1,0,-1,0,0,-1,0,0,0,-1,0,-1,-1,-1,-1,-1,0,-1,0,-1],
-    [-1,0,-1,0,-1,-1,0,0,0,-1,0,0,0,0,0,0,0,0,0,-1],
-    [-1,0,-1,-2,-1,0,0,0,-1,-1,0,-1,-1,-1,-1,-1,0,-1,0,-1],
-    [-1,0,-1,-1,-1,0,0,0,-1,0,0,-1,0,0,0,-1,0,0,0,-1],
-    [-1,0,0,0,0,0,0,0,-1,-1,-1,-1,0,0,0,-1,0,-1,0,-1],
-    [-1,0,0,0,0,0,0,0,0,0,-1,-1,0,0,-1,-1,0,0,0,-1],
-    [-1,0,-1,-1,-1,-1,-1,0,0,0,-1,-1,0,0,-1,0,0,-1,0,-1],
-    [-1,0,-1,0,0,0,-1,0,0,0,-1,-1,0,0,0,0,0,0,0,-1],
-    [-1,0,-1,-1,-1,0,-1,-1,-1,-1,-1,-1,0,0,0,-1,0,-1,0,-1],
-    [-1,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,-1,0,0,0,-1],
-    [-1,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,-1,0,-1],
-    [-1,0,-1,-1,0,-1,-1,0,-1,-1,-1,-1,-1,-1,-1,0,0,0,0,-1],
-    [-1,0,-1,-1,0,-1,-1,0,0,0,0,-1,-1,-1,-1,0,0,-1,0,-1],
-    [-1,0,0,0,0,0,0,0,0,0,0,-1,-1,-1,-1,-1,0,0,0,-1],
-    [-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,-1,0,-1],
-    [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+maze = [
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,2,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,0,1],
+    [1,0,1,1,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,1],
+    [1,0,1,0,0,1,0,0,0,1,0,1,1,1,1,1,0,1,0,1],
+    [1,0,1,0,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,3,1,0,0,0,1,1,0,1,1,1,1,1,0,1,0,1],
+    [1,0,1,1,1,0,0,0,1,0,0,1,0,0,0,1,0,0,0,1],
+    [1,0,0,0,0,0,0,0,1,1,1,1,0,0,0,1,0,1,0,1],
+    [1,0,0,0,0,0,0,0,0,0,1,1,0,0,1,1,0,0,0,1],
+    [1,0,1,1,1,1,1,0,0,0,1,1,0,0,1,0,0,1,0,1],
+    [1,0,1,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,1],
+    [1,0,1,1,1,0,1,1,1,1,1,1,0,0,0,1,0,1,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,1],
+    [1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,0,0,0,0,1],
+    [1,0,1,1,0,1,1,0,0,0,0,1,1,1,1,0,0,1,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
     ]
 
-path=[]
+class Block:
+    def __init__(self, value):
+        self.value=value
+        self.visited=False
+
+region=[]  #an array of blocks
+
+for i in range(len(maze)):
+    reglist=[]
+    for j in range(len(maze)):
+        reglist.append(Block(maze[i][j]))
+    region.append(reglist)
 
 
-def printRegion(region):
+
+def printRegion():
     for i in range(len(region)):
         for j in range(len(region)):
-            if(region[i][j]==-1):
+            if(region[i][j].value==1):
                 print("x", end=" ", flush=True)
-            elif (region[i][j]== 0):
+            elif (region[i][j].value == 0):
                 print(" ", end=" ", flush=True)
-            elif(region[i][j]== -3 or region[i][j] == -2):
+            elif(region[i][j].value == 2 or region[i][j].value == 3):
                 print("O", end=" ", flush=True)
             else:
-                print(region[i][j],end=" ", flush=True)     
+                print(region[i][j].value,end=" ", flush=True)     
         print("...")
 
-def printPath(path):
-    for element in path:
-        print('path:', element)
+def drawPath(endValue, x,y):
 
-def markNode(x,y,number):
+    if(x<0 or x>19 or y<0 or y>19): return False
+
+    if(region[x][y].visited or region[x][y].value==1): return False
+
+    if(region[x][y].value == endValue): return True
+
+    region[x][y].visited = True
     
-    coords = [{'x':x+1,'y':y}, {'x':x-1,'y':y},{'x':x,'y':y+1},{'x':x,'y':y-1}]
+    region[x][y].value = 7
 
-    for coord in coords:
-        if(possibleCoord(coord['x'],coord['y']) and region[coord['x']][coord['y']]==0):
-            print('region coord x', coord['x'],'y', coord['y'])
-            printRegion(region)
-            region[coord['x']][coord['y']]=number
+    if drawPath(endValue,x+1,y): return True
 
+    if drawPath(endValue,x-1,y): return True
 
+    if drawPath(endValue,x,y+1): return True
 
+    if drawPath(endValue,x,y-1): return True
 
-# Mark the possible paths between -3 and -2 and mark them
+    region[x][y].value=0
+    region[x][y].visited = False
 
-def markPath(start,finish,x,y,number):
-
-    if(possibleCoord(x,y) and region[x][y] != finish):
-        markNode(x,y,number)
-
-        coords = [{'x':x+1,'y':y}, {'x':x-1,'y':y},{'x':x,'y':y+1},{'x':x,'y':y-1}]
-        for coord in coords:
-            if(possibleCoord(coord['x'],coord['y']) and number<30):
-                printRegion(region)
-                markPath(start,finish,coord['x'],coord['y'],number+1)
-    
-    return 
+    printRegion()
+    return False
 
 
+
+printRegion()
+drawPath(3,1,1)
+printRegion()
     
 
 
-#check if coordinate is possible
-def possibleCoord(x,y):
-    if(x < 0 or  x>19 or y>19 or y<0 or region[x][y]==-1): return False
-    return True
-
-#return coordinate of the min value
-def selectMin(x,y):
-
-    minCoord={'x':0,'y':0,'val':sys.maxsize}
-
-    coords = [{'x':x+1,'y':y}, {'x':x-1,'y':y},{'x':x,'y':y+1},{'x':x,'y':y-1}]
-    for coord in coords:     
-        if(possibleCoord(coord['x'],coord['y']) and region[coord['x']][coord['y']]<minCoord['val'] and region[coord['x']][coord['y']]!=-1): 
-            minCoord['x']= coord['x']
-            minCoord['y'] = coord['y']
-            minCoord['val'] = region[coord['x']][coord['y']]
-            print('minCoord',minCoord)
-
-    return minCoord
-    
-#store min coordinate in path list
-def storeMinPath(start,endx,endy):
-    
-    if(region[endx][endy]==start): return
-    
-    minCoord=selectMin(endx,endy)
-
-    path.append(minCoord)
-
-    print('minCoord Final',minCoord)
-
-    #storeMinPath(start,minCoord['x'],minCoord['y'])
 
 
 
 
 
-    
-
-#Function calls
-printRegion(region)
-markPath(-3,-2,1,1,1)
-storeMinPath(-3,6,3)
-printRegion(region)
-#printPath(path)
 
 
  
