06d3cb22e4a806177ad4fc2d46fd671670de97a4
Maria V <mariatua2@gmail.com>
2019-04-16 19:01:50 -0400

    failing at this approach

--- reto2.py
+++ reto2.py
@@ -2,7 +2,7 @@
 
 import sys
 
-#Objective: Find the shortest path between -3 and -2
+#Objective: Find the shortest path between 3 and 2
 
 #Note: This is python3 
 
@@ -57,34 +57,62 @@ def printRegion():
                 print(region[i][j].value,end=" ", flush=True)     
         print("...")
 
-def drawPath(endValue, x,y):
+def drawPath(endValue, x,y,region):
 
-    if(x<0 or x>19 or y<0 or y>19): return False
+    if(x<0 or x>19 or y<0 or y>19): return 0
 
-    if(region[x][y].visited or region[x][y].value==1): return False
+    if(region[x][y].visited or region[x][y].value==1): return 0
 
-    if(region[x][y].value == endValue): return True
+    if(region[x][y].value == endValue): return 1
 
     region[x][y].visited = True
+    region[x][y].value='.'
     
-    region[x][y].value = 7
 
-    if drawPath(endValue,x+1,y): return True
+    dummy = region
+    
+    up=0
+    down=0
+    right=0
+    left=0
 
-    if drawPath(endValue,x-1,y): return True
+    if drawPath(endValue,x+1,y,dummy): down = 1+drawPath(endValue,x+1,y,dummy)
+    if drawPath(endValue,x-1,y,dummy): up = 1+drawPath(endValue,x-1,y,dummy)
+    if drawPath(endValue,x,y+1,dummy): right = 1+drawPath(endValue,x,y+1,dummy)
+    if drawPath(endValue,x,y-1,dummy): left = 1+drawPath(endValue,x,y-1,dummy)
 
-    if drawPath(endValue,x,y+1): return True
 
-    if drawPath(endValue,x,y-1): return True
+    #select max
 
+    directions = {'d': down, 'u': up, 'r': right, 'l': left}
+    direction= min(directions, key=directions.get)
+    
+    print('direction',direction)
+
+      
+    if direction=='d':  
+        return 1+drawPath(endValue,x+1,y,region)
+
+    if direction=='u':  
+        return 1+drawPath(endValue,x-1,y,region)
+
+    if direction=='r':  
+        return 1+drawPath(endValue,x,y+1,region)
+
+    if direction=='l':  
+        return 1+drawPath(endValue,x,y-1,region)
+
+  
     region[x][y].value=0
     region[x][y].visited = False
-    return False
+    return 0
+
+    
 
 
 
 printRegion()
-drawPath(3,1,1)
+drawPath(3,1,1,region)
 print("")
 printRegion()
     
