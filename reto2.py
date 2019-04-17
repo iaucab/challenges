1920e23c769626d9ac93b5cac0ed0f9f160b9c88
Jose Manuel Romero <josemanuelromerop@gmail.com>
2019-04-17 15:16:22 -0400

    Add currentDeep and maxDeep in function drawPath()
    Add minDeep in class Block

--- reto2.py
+++ reto2.py
@@ -32,7 +32,7 @@ maze = [
 class Block:
     def __init__(self, value):
         self.value=value
-        self.visited=False
+        self.minDeep = None
 
 region=[]  #an array of blocks
 
@@ -54,37 +54,43 @@ def printRegion():
             elif(region[i][j].value == 2 or region[i][j].value == 3):
                 print("O", end=" ", flush=True)
             else:
-                print(region[i][j].value,end=" ", flush=True)     
+                print(region[i][j].value,end=" ", flush=True)
         print("...")
 
-def drawPath(endValue, x,y):
-
+def drawPath(endValue, x,y, currentDeep, maxDeep):
     if(x<0 or x>19 or y<0 or y>19): return False
-
-    if(region[x][y].visited or region[x][y].value==1): return False
-
-    if(region[x][y].value == endValue): return True
-
-    region[x][y].visited = True
     
-    region[x][y].value = '.'
+    if(region[x][y].value==1): return False
+    if(region[x][y].value == endValue): return True
+    
+    if region[x][y].minDeep is None: region[x][y].minDeep = currentDeep
+    elif region[x][y].minDeep > currentDeep: return False
+    elif region[x][y].minDeep < currentDeep: region[x][y].minDeep = currentDeep
+    if not (currentDeep > maxDeep): return False
 
-    if drawPath(endValue,x+1,y): return True
-
-    if drawPath(endValue,x-1,y): return True
-
-    if drawPath(endValue,x,y+1): return True
-
-    if drawPath(endValue,x,y-1): return True
-
+    if drawPath(endValue,x+1,y, currentDeep-1, maxDeep): 
+        region[x][y].value = '.'
+        return True
+    if drawPath(endValue,x-1,y, currentDeep-1, maxDeep): 
+        region[x][y].value = '.'
+        return True
+    if drawPath(endValue,x,y+1, currentDeep-1, maxDeep): 
+        region[x][y].value = '.'
+        return True
+    if drawPath(endValue,x,y-1, currentDeep-1, maxDeep): 
+        region[x][y].value = '.'
+        return True
     region[x][y].value=0
-    region[x][y].visited = False
     return False
 
 
 
 printRegion()
-drawPath(3,1,1)
+wasFound = False
+maxDeep = 0
+while not wasFound:
+    maxDeep -= 1
+    wasFound = drawPath(3,1,1,0,maxDeep)
 print("")
 printRegion()
 
@@ -94,4 +100,3 @@ printRegion()
 
 
 
- 
